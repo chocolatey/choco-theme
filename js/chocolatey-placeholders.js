c255432d0367eea0e694bb314f4fa4dc9ee68647
Stephanie Burrows <designxsteph@gmail.com>
2020-12-05 20:30:38 -0600

    (theme) Add Backgrounds and Placeholders
    Adds several images that can be used as background images via css. In
    addition, the css/js needed to support randomized placeholders on the
    blog has been added and will use these background images.

---
+++ js/chocolatey-placeholders.js
@@ -0,0 +1,30 @@
+$(function() {
+    var placeholderContainer = $('.placeholder-container');
+
+    placeholderContainer.each(function(){
+        var placeholderImg = $(this).find('div:first-child');
+        var placeholderBg = $(this).find('div:last-child');
+        var bgColor = [
+            'bg-primary', 
+            'bg-primary',
+            'bg-success', 
+            'bg-success',
+            'bg-danger',
+            'bg-purple'
+            ];
+        var bgImg = [
+            'bg-sun-tornado', 
+            'bg-liquid-cheese',
+            'bg-wintery-sunburst',
+            'bg-bullseye-gradient',
+            'bg-geometric-intersection',
+            'bg-cornered-stairs'
+            ];
+
+        // Add background color
+        placeholderBg.addClass(bgColor[~~(Math.random()*bgColor.length)]);
+
+        // Add background img
+        placeholderImg.addClass(bgImg[~~(Math.random()*bgImg.length)]);
+    });
+});
