a4ca758b5d1861987440c1a68292b3c0fc92feac
Stephanie Hays <steph@stephhays.dev>
2022-03-15 10:49:15 -0500

    Add Birthday Bits
    Adds the following:
    * Top alert banner
    * New post in right side flyout
    * Global logos
    * Confetti

---
+++ js/chocolatey-birthday.js
@@ -0,0 +1,39 @@
+(function() {
+    var birthdayCookie = 'chocolatey_11_birthday';
+
+    if (getCookie(birthdayCookie)) {
+        return;
+    }
+
+    if (~location.hostname.indexOf('chocolatey.org')) {
+        document.cookie = birthdayCookie + '=true; ' + setCookieExpirationNever() + 'path=/; domain=chocolatey.org;';
+    } else {
+        document.cookie = birthdayCookie + '=true;' + setCookieExpirationNever() + 'path=/;';
+    }
+
+    confettiBurst();
+
+    function confettiBurst() {
+        var duration = 2 * 1000;
+        var animationEnd = Date.now() + duration;
+        var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0, disableForReducedMotion: true };
+
+        function randomInRange(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        var interval = setInterval(function() {
+            var timeLeft = animationEnd - Date.now();
+
+            if (timeLeft <= 0) {
+                return clearInterval(interval);
+            }
+
+            var particleCount = 50 * (timeLeft / duration);
+            
+            // since particles fall down, start a bit higher than random
+            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+        }, 250);
+    }
+})();
