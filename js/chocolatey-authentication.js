9c1c450fd7072f98918e904640f8ab0406da126f
Stephanie Hays <steph@stephhays.dev>
2023-01-10 15:11:13 -0600

    (#293)(#294) Revamp Login and Account Navigation
    The login and account buttons on community.chocolatey.org have been
    moved into the main navigation area. Having them in this area makes the
    experience more intuitive and familiar. Navigating to the Account page
    from the first page of packages (or any page) will now take the user to
    the correct page instead of a broken link. The url for the account page
    is now being inserted with JS when the user is authenticated instead of
    in the C# page. Due to caching, this needs to be done on the client
    side to ensure the proper link is available.

--- js/chocolatey-authentication.js
+++ js/chocolatey-authentication.js
@@ -3,30 +3,33 @@ import jQuery from 'jquery';
 (() => {
     // Set Login/Logoff Navigation
     const authenticationSuccess = data => {
-        const authenticated = document.getElementsByTagName('BODY')[0];
-
         if (data.isAuthenticated) {
-            authenticated.classList.add('user-authenticated');
+            const authenticatedName = document.querySelectorAll('.user-authenticated-name');
+
+            document.body.classList.add('user-authenticated');
+
+            for (const i of authenticatedName) {
+                if (i.nodeName === 'A') {
+                    i.setAttribute('href', `/profiles/${data.userName}`);
+                } else {
+                    i.innerText = data.userName;
+                }
+            }
+        } else {
+            document.body.classList.add('user-unauthenticated');
         }
     };
 
-    jQuery(() => {
-        // Only check authentication on certain parts of the site
-        const authenticatedURL =
-            window.location.href.indexOf('/packages') > -1 ||
-            window.location.href.indexOf('/courses') > -1 ||
-            window.location.href.indexOf('/account') > -1 ||
-            window.location.href.indexOf('/profiles') > -1 ||
-            window.location.href.indexOf('/admin') > -1;
+    const authenticationError = () => {
+        document.body.classList.add('user-unauthenticated');
+    };
 
-        if (authenticatedURL) {
-            jQuery.ajax({
-                type: 'POST',
-                url: `${window.location.protocol}//${window.location.host}`,
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'json',
-                success: authenticationSuccess
-            });
-        }
+    jQuery.ajax({
+        type: 'POST',
+        url: `${window.location.protocol}//${window.location.host}`,
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: authenticationSuccess,
+        error: authenticationError
     });
 })();
