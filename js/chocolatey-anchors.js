8e5d69fbc9c201f9d7e4c636be75cb6d507824f5
Stephanie Hays <steph@stephhays.dev>
2023-05-18 13:18:26 -0500

    (#330) Styles to enhance docs.chocolatey.org
    In order to update the styles on docs.chocolatey.org, a few new CSS and
    JS changes were made. A new class of `d-anchor-none` can now be added
    to any `<h1>` through `<h6>` to remove the anchor that would otherwise
    be added by anchor.js.

--- js/chocolatey-anchors.js
+++ js/chocolatey-anchors.js
@@ -5,5 +5,5 @@ import AnchorJS from 'anchor-js';
     const anchors = new AnchorJS();
 
     anchors.options.placement = 'left';
-    anchors.add('#mainContent h2, #mainContent h3, #mainContent h4, #mainContent h5, #mainContent h6');
+    anchors.add('#mainContent h2:not(.d-anchor-none), #mainContent h3:not(.d-anchor-none), #mainContent h4:not(.d-anchor-none), #mainContent h5:not(.d-anchor-none), #mainContent h6:not(.d-anchor-none)');
 })();
