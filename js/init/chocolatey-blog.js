571b7b51f8607c580b2173b5683ca43638968e64
Stephanie Hays <steph@stephhays.dev>
2023-07-27 16:18:46 -0500

    (#347) Add AnchorJS anchors to list items
    This adds anchors to list items that contain a <strong> or a <code>
    node directly after an opening <li> tag. This is to make it easier to
    navigate to these items, as often times this format is used more as a
    "definition", or something we'd want to potentially link to.
    
    The order of the JS imports needed to be modified to ensure that the
    markdown conversion JS for list items was complete before trying to
    detect the desired nodes inside the list items.

--- js/init/chocolatey-blog.js
+++ js/init/chocolatey-blog.js
@@ -1,11 +1,11 @@
 import { Alert, Button, Carousel, Collapse } from 'bootstrap';
 import '../lib/prism.min.js';
 import '../chocolatey-alerts.js';
-import '../chocolatey-anchors.js';
 import '../chocolatey-callouts.js';
 import '../ts/chocolatey-code.js';
 import '../chocolatey-collapse-nested.js';
 import '../chocolatey-markdown.js';
+import '../chocolatey-anchors.js'; // This is dependent on the markdown script above
 import '../chocolatey-placeholders.js';
 import '../chocolatey-sticky-table.js';
 import '../chocolatey-substrings.js';
