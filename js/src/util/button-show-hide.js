60300ce527c59b77631ead45e5f1be8b61fc7523
st3phhays <steph@stephhays.dev>
2025-01-15 05:31:59 -0600

    (#430) Table interactive select functionality
    While this doesn't remove the jQuery Duallistbox library yet, this is
    the start of being able to do so. The changes here in combination with
    others in the repository allow for similar functionality, yet in an
    accessible way. This introduces the fuse.js library to help with search.

---
+++ js/src/util/button-show-hide.js
@@ -0,0 +1,15 @@
+export const showHideBtn = () => {
+    const btnShowHide = document.querySelectorAll('.btn-show-hide');
+
+    if (btnShowHide) {
+        btnShowHide.forEach(el => {
+            el.addEventListener('click', () => {
+                if (el.textContent.includes('Show')) {
+                    el.textContent = el.textContent.replace('Show', 'Hide');
+                } else if (el.textContent.includes('Hide')) {
+                    el.textContent = el.textContent.replace('Hide', 'Show');
+                }
+            });
+        });
+    }
+};
