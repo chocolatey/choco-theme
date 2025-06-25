bf558a935a2903272bda2aa7737dd1ae84e9618a
Steph Hays <steph@stephhays.dev>
2025-06-25 07:49:10 -0500

    (#460) Utils to support credits generation
    These utils will help support the generation of credit files, however
    they can be used in multiple projects for other reasons. The to-id
    function converts a string to a valid Id. The get-unique-id function
    ensures there are no duplicate Id's on a given page, and follows the
    common format of adding numbers, like in a toc, to the end. The
    regenerate-modal function will trigger a "loading" or "regenerating"
    modal on a button click. This is useful to stop user interaction
    while a page is "thinking".

---
+++ js/src/ts/util/to-id.ts
@@ -0,0 +1,14 @@
+export const toId = (str: string): string => {
+    const id = str
+        .toLowerCase()
+        .replaceAll(' ', '-')           // Replace all spaces with hyphens
+        .replaceAll('.', '')            // Remove all dots
+        .replaceAll('@', '')            // Remove all @ signs
+        .replaceAll('/', '')            // Remove all / signs
+        .replace(/^-+|-+$/g, '')        // Trim leading/trailing hyphens
+        .replace(/--+/g, '-');          // Replace multiple hyphens with a single one
+
+    // if (!/^[a-z]/.test(id)) id = `id-${id}`; // Ensure starts with letter
+
+    return id;
+};
