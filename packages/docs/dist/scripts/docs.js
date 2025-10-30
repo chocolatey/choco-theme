76989e9160b58380df2464c700a6458655d6a273
Steph Hays <steph@stephhays.dev>
2025-10-30 09:07:46 -0500

    (#478) Add validation to DynamicCodeBlocks
    This adds a new component that can be used along side the
    DynamicCodeBlock component. This new component will add validation
    to the inputs, and show warnings when users havn't entered any of their
    own information. This will help so that users don't accidently copy
    code snippets before they enter in their own variables.

--- packages/docs/dist/scripts/docs.js
+++ packages/docs/dist/scripts/docs.js
@@ -14455,6 +14455,7 @@
   document.addEventListener("DOMContentLoaded", () => {
     const dynamicCodeBlockContainers = document.querySelectorAll(".dynamic-code-block-container");
     const dynamicCodeBlockInputs = document.querySelectorAll(".dynamic-code-block-input");
+    const dynamicCodeBlockValidation = document.querySelectorAll(".dynamic-code-block-validation");
     const replaceCodeVariableInCodeBlock = (input, inputVariable, inputDefaultValue, inputCookie) => {
       const codeVariables = document.querySelectorAll(`.${inputVariable}`);
       const inputValue = inputCookie ? inputCookie : input.value;
@@ -14473,6 +14474,30 @@
         }
       }
     };
+    const validateInputs = () => {
+      dynamicCodeBlockValidation.forEach((validation) => {
+        const inputNames = validation.getAttribute("data-input-names").split(",");
+        let allFilled = true;
+        for (const inputName of inputNames) {
+          const input = document.querySelector(`.dynamic-code-block-input[name="${inputName}"]`);
+          if (!input.value) {
+            allFilled = false;
+            input.classList.add("is-invalid");
+          } else {
+            input.classList.remove("is-invalid");
+          }
+        }
+        if (allFilled) {
+          validation.classList.remove("show");
+          setTimeout(() => {
+            validation.classList.add("d-none");
+          }, 150);
+        } else {
+          validation.classList.remove("d-none");
+          validation.classList.add("show");
+        }
+      });
+    };
     dynamicCodeBlockInputs.forEach((input) => {
       const inputVariable = input.name;
       const inputDefaultValue = input.getAttribute("data-default-value");
@@ -14482,8 +14507,10 @@
         dynamicCodeBlockContainer.innerHTML = dynamicCodeBlockContainer.innerHTML.replaceAll(regex, `<span class="${inputVariable}">${inputCookie ? inputCookie : inputDefaultValue}</span>`);
       }
       replaceCodeVariableInCodeBlock(input, inputVariable, inputDefaultValue, inputCookie);
+      validateInputs();
       input.addEventListener("keyup", () => {
         replaceCodeVariableInCodeBlock(input, inputVariable, inputDefaultValue);
+        validateInputs();
       });
     });
     copyCodeBlocks();
--- src/scripts/dynamic-code-block.js
+++ src/scripts/dynamic-code-block.js
@@ -5,6 +5,7 @@ import { setCookie } from '@choco-core/util/set-cookie';
 document.addEventListener('DOMContentLoaded', () => {
     const dynamicCodeBlockContainers = document.querySelectorAll('.dynamic-code-block-container');
     const dynamicCodeBlockInputs = document.querySelectorAll('.dynamic-code-block-input');
+    const dynamicCodeBlockValidation = document.querySelectorAll('.dynamic-code-block-validation');
 
     const replaceCodeVariableInCodeBlock = (input, inputVariable, inputDefaultValue, inputCookie) => {
         const codeVariables = document.querySelectorAll(`.${inputVariable}`);
@@ -28,6 +29,35 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    const validateInputs = () => {
+        dynamicCodeBlockValidation.forEach(validation => {
+            const inputNames = validation.getAttribute('data-input-names').split(',');
+            let allFilled = true;
+
+            for (const inputName of inputNames) {
+                const input = document.querySelector(`.dynamic-code-block-input[name="${inputName}"]`);
+
+                if (!input.value) {
+                    allFilled = false;
+                    input.classList.add('is-invalid');
+                    // break;
+                } else {
+                    input.classList.remove('is-invalid');
+                }
+            }
+
+            if (allFilled) {
+                validation.classList.remove('show');
+                setTimeout(() => {
+                    validation.classList.add('d-none');
+                }, 150);
+            } else {
+                validation.classList.remove('d-none');
+                validation.classList.add('show');
+            }
+        });
+    };
+
     dynamicCodeBlockInputs.forEach(input => {
         const inputVariable = input.name;
         const inputDefaultValue = input.getAttribute('data-default-value');
@@ -39,9 +69,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         replaceCodeVariableInCodeBlock(input, inputVariable, inputDefaultValue, inputCookie);
+        validateInputs();
 
         input.addEventListener('keyup', () => {
             replaceCodeVariableInCodeBlock(input, inputVariable, inputDefaultValue);
+            validateInputs();
         });
     });
 
